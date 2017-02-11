5e3a05ea52bbfcde34ea0921bb223bf85bbb3a11
John Gliksberg <jg.trosh@gmail.com>
2017-02-11 01:15:07 +0100

    major update (cut/uncut/write, layout, ...)

--- nano.py
+++ nano.py
@@ -3,28 +3,43 @@
 import curses
 
 # vertical offset
-VOFF = 1
-HOFF = 4
+VOFF = 2
+HOFF = 0
+
+def cvToChar(cv):
+    # cv = [char, vis]
+    if cv[1]:
+        return cv[0]
+    return ""
+
+commands = [
+    " Exit",
+    " Write Out",
+    " Toggle Hidden",
+    " Toggle Debug",
+    " Cut (TODO)",
+    " Uncut (TODO)"
+]
 
 class Editor():
     line = 0
     char = 0
-    # TODO displine = 0
+    displine = 0
     dispchar = 0
     userchar = 0
     showHidden = False
+    filename = False
+    cutbuffer = []
+    cutting = 0
 
     def updateCursor(self):
-        #self.stdscr.addstr(
-        #    1, 0,
-        #    "line: {}, col: {}".format(
-        #        self.line, self.char),
-        #    curses.color_pair(2))
-        #self.stdscr.addstr(
-        #    2, 0,
-        #    "displine: {}, dispcol: {}, usercol: {}".format(
-        #        self.line, self.dispchar, self.userchar),
-        #    curses.color_pair(2))
+        wh, ww = self.stdscr.getmaxyx()
+        if self.line + VOFF >= wh:
+            # TODO whaat
+            return
+        if self.dispchar + HOFF >= ww:
+            # TODO whhaat
+            return
         self.stdscr.move(self.line + VOFF, self.dispchar + HOFF)
 
     def setChar(self, userchar):
@@ -132,7 +147,7 @@ class Editor():
         self.char += 1
         self.dispchar += 1
         self.userchar = self.dispchar
-        self.displayLine(self.line)
+        self.displayLine()
 
     def newline(self):
         ln = self.line
@@ -155,10 +170,10 @@ class Editor():
                 self.dispchar = self.char = -1
             else:
                 self.decChar()
-            self.text[self.line:self.line+2] = [
-                self.text[self.line] + self.text[self.line+1]]
-            self.vis[self.line:self.line+2] = [
-                self.vis[self.line] + self.vis[self.line+1]]
+            self.text[self.line] += self.text[self.line+1]
+            del self.text[self.line+1]
+            self.vis[self.line] += self.vis[self.line+1]
+            del self.vis[self.line+1]
             self.incChar()
             self.display()
         elif sum(self.vis[self.line][:self.char]) == 1:
@@ -166,79 +181,177 @@ class Editor():
             self.vis[self.line][self.char] = False
             self.dispchar = self.char = -1
             self.incChar()
-            self.displayLine(self.line)
+            self.displayLine()
         else:
             self.decChar()
             self.vis[self.line][self.char] = False
             self.decChar()
             self.incChar()
-            self.displayLine(self.line)
+            self.displayLine()
 
-    def display(self):
-        self.stdscr.clear()
-        self.stdscr.addstr(
-            0, 0,
-            "^c to quit, ^H to toggle hidden content, arrows to move",
-            curses.color_pair(2))
-        for ln, line in enumerate(self.text):
-            vcn = 0
-            self.stdscr.addstr(
-                ln + VOFF, 0,
-                "{:3d}".format(ln))
-            for cn, c in enumerate(line[:-1]):
-                if self.vis[ln][cn]:
-                    self.stdscr.addch(ln + VOFF, vcn + HOFF, c)
-                    vcn += 1
-                elif self.showHidden:
-                    self.stdscr.addstr(
-                        ln + VOFF, vcn + HOFF,
-                        c, curses.color_pair(1))
-                    vcn += 1
-        self.updateCursor()
+    def getVisible(self, ln):
+        return "".join(map(cvToChar, zip(self.text[ln], self.vis[ln])))
 
-    def displayLine(self, ln):
+    def cut(self):
+        if self.cutting == 1:
+            self.cutbuffer.append(self.getVisible(self.line))
+            if len(self.vis) == 1 and sum(self.vis[0]) == 1:
+                self.cutting = 2
+        else:
+            self.cutbuffer = [self.getVisible(self.line)]
+            self.cutting = 1
+        self.vis[self.line] = [False for n in self.text[self.line]]
+        if self.line + 1 < len(self.text):
+            self.text[self.line] += self.text[self.line+1]
+            del self.text[self.line+1]
+            self.vis[self.line] += self.vis[self.line+1]
+            del self.vis[self.line+1]
+            self.char = len(self.text[self.line]) - 1
+            if self.showHidden:
+                self.dispchar = self.char
+            else:
+                self.dispchar = 0
+            self.incChar()
+        else:
+            self.vis[self.line][-1] = True
+            if self.line > 0:
+                self.text[self.line-1] += self.text[self.line]
+                del self.text[self.line]
+                self.vis[self.line-1][-1] = False
+                self.vis[self.line-1] += self.vis[self.line]
+                del self.vis[self.line]
+                self.line -= 1
+            self.char = len(self.text[self.line]) - 1
+            if self.showHidden:
+                self.dispchar = self.char
+            else:
+                self.dispchar = sum(self.vis[self.line]) - 1
+        self.display()
+
+    def uncut(self):
+        self.text[self.line:self.line] = self.cutbuffer
+        self.vis[self.line:self.line] = \
+            [[True for c in line] for line in self.cutbuffer]
+        self.line += len(self.cutbuffer)
+        self.userchar = self.dispchar = self.char = 0
+        self.display()
+
+    def displayline(self, ln):
+        wh, ww = self.stdscr.getmaxyx()
+        #self.stdscr.addstr(ln + VOFF, 0, "{:3d}".format(ln))
         vcn = 0
-        self.stdscr.move(ln + VOFF, 0)
-        self.stdscr.clrtoeol()
-        self.stdscr.addstr(ln + VOFF, 0, "{:3d}".format(ln))
         for cn, c in enumerate(self.text[ln][:-1]):
+            if cn + HOFF == ww:
+                break
             if self.vis[ln][cn]:
                 self.stdscr.addch(ln + VOFF, vcn + HOFF, c)
                 vcn += 1
             elif self.showHidden:
-                self.stdscr.addstr(
+                if c == "\n":
+                    c = "↵"
+                self.stdscr.addch(
                     ln + VOFF, vcn + HOFF,
                     c, curses.color_pair(1))
                 vcn += 1
+
+    def display(self):
+        self.stdscr.clear()
+        self.stdscr.addstr(
+            0, 0, "  NoHide nano", curses.A_REVERSE)
+        self.stdscr.chgat(curses.A_REVERSE)
+        wh, ww = self.stdscr.getmaxyx()
+        for n in range(0, len(commands), 2):
+            if (n+1)*10 > ww:
+                break
+            c = commands[n].split(" ", 1)
+            self.stdscr.addstr(wh-2, n*10, c[0], curses.A_REVERSE)
+            self.stdscr.addstr(" " + c[1])
+            if n + 1 == len(commands):
+                break
+            c = commands[n+1].split(" ", 1)
+            self.stdscr.addstr(wh-1, n*10, c[0], curses.A_REVERSE)
+            self.stdscr.addstr(" " + c[1])
+        for ln in range(len(self.text)):
+            self.displayline(ln)
         self.updateCursor()
 
+    def displayLine(self):
+        self.stdscr.move(self.line + VOFF, 0)
+        self.stdscr.clrtoeol()
+        self.displayline(self.line)
+        self.updateCursor()
+
+    def write(self):
+        if not self.filename:
+            self.stdscr.attron(curses.A_REVERSE)
+            self.stdscr.addstr(20, 0, "File Name to Write ")
+            if self.showHidden:
+                self.stdscr.addstr("(with hidden content) : ")
+            else:
+                self.stdscr.addstr("(without hidden content) : ")
+            curses.echo()
+            self.filename = self.stdscr.getstr()
+            curses.noecho()
+            self.stdscr.attroff(curses.A_REVERSE)
+        with open(self.filename, "w") as f:
+            if self.showHidden:
+                for n, line in enumerate(self.text):
+                    f.write(line)
+                    f.write("".join([str(v+0) for v in self.vis[n]])+"\n")
+            else:
+                for n in range(len(self.text))
+                    f.write(self.visible(n))
+        self.display()
 
 def main(stdscr):
     stdscr.clear()
     curses.init_pair(1, curses.COLOR_RED,   curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    e = Editor(stdscr, ["hello\n","\n","world\n"])
-    e.vis[0][-2] = False
+    e = Editor(stdscr, ["\n"])
+    debug = False
     e.display()
     stdscr.refresh()
     while True:
         try:
-            c = stdscr.getch()
-            #stdscr.addstr(0, 20, "[{}]".format(c))
-            if c == 263: # ^H
+            c = stdscr.getkey()
+            if c not in ["", "KEY_RESIZE"]:
+                e.cutting = 0
+            if c == "KEY_RESIZE":
+                e.display()
+            elif c == "":
                 e.toggleHidden()
                 stdscr.clear()
                 e.display()
-            elif c == curses.KEY_RIGHT: e.incChar()
-            elif c == curses.KEY_LEFT:  e.decChar()
-            elif c == curses.KEY_UP:    e.setLine(e.line - 1)
-            elif c == curses.KEY_DOWN:  e.setLine(e.line + 1)
-            elif c in [127, curses.KEY_BACKSPACE]:
+            elif c == "":
+                debug = not debug
+                stdscr.addstr(0, 20, "[{}]".format(c))
+                e.display()
+            elif c == "^O":
+                e.write()
+            elif c == "":
+                e.cut()
+            elif c == "":
+                stdscr.addstr(1, 40, "^U")
+                e.uncut()
+            elif c == "KEY_RIGHT": e.incChar()
+            elif c == "KEY_LEFT":  e.decChar()
+            elif c == "KEY_UP":    e.setLine(e.line - 1)
+            elif c == "KEY_DOWN":  e.setLine(e.line + 1)
+            elif c == "":
                 e.backspace()
-            elif c == 10:
+            elif c == "\n":
                 e.newline()
-            else:
-                e.type(chr(c))
+            elif len(c) == 1:
+                e.type(c)
+            if debug:
+                stdscr.addstr(0, 20, "[{}]".format(c))
+                stdscr.clrtoeol()
+                stdscr.chgat(curses.A_REVERSE)
+                stdscr.addstr(0, 35, "c{},d{},u{}".format(
+                        e.char, e.dispchar, e.userchar))
+                stdscr.clrtoeol()
+                stdscr.chgat(curses.A_REVERSE)
+                e.updateCursor()
             stdscr.refresh()
         except KeyboardInterrupt:
             break
